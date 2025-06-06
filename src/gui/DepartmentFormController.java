f6cb554d0424b108347d0d4388934d1c372dc4c9
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-06-06 09:02:17 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,50 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable{
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNode();
+	}
+	
+	private void initializeNode() {
+		Constraints.setTextFieldInteger(txtId);  // O campo Id so aceita numero inteiro
+		Constraints.setTextFieldMaxLength(txtName, 30);  // O campo nome aceita no maximo 30 caracteres
+	}
+
+}
